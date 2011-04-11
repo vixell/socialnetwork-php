342bcfc693bd9e23e23344b7391cb6def70f3fc1
remiheens <remiheens@gmail.com>
2011-04-11 19:23:57 +0000

    sd

--- getToken/twitter.inc.php
+++ getToken/twitter.inc.php
@@ -1,5 +1,5 @@
 <?php
-define("CONSUMER_KEY", "VOTRE_CONSUMER_KEY");
-define("CONSUMER_SECRET", "VOTRE_CONSUMER_SECRET");
-define("OAUTH_CALLBACK", "callback.php");
+define("CONSUMER_KEY", "");
+define("CONSUMER_SECRET", "");
+define("OAUTH_CALLBACK", "http://name.tld/twitter.callback.php");
 ?>
